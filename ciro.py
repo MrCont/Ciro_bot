fa1d2c5cec2138dc4fff21cfb2055c475a444f6d
MrCont <63643248+MrCont@users.noreply.github.com>
2021-03-24 12:56:19 +0100

    initial_commit

---
+++ ciro.py
@@ -0,0 +1,41 @@
+import mp3
+import discord
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+import random
+from gtts import gTTS
+def dillo_in_napoletano(_str):
+    return gTTS(_str,lang="it")
+bot = commands.Bot(command_prefix=">")
+
+with open("proverbi_sfaccimm.txt","r",encoding="utf-8") as f:
+    lines = f.readlines() 
+    lines = list(lines)
+    
+@bot.event
+async def on_ready():
+    print ("Ready")
+
+@bot.command(pass_context=True)
+async def join(ctx):
+    if  (ctx.author.voice):
+        channel=ctx.message.author.voice.channel
+        global voice
+        voice = await channel.connect()
+    else:
+        await ctx.send("not in a voice channel")
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+@bot.command()
+async def proverbio(ctx):
+    msg=dillo_in_napoletano("O' proverbio dice:"+random.choice(lines)[4:])
+    msg.save("msg.mp3")
+    print("playing msg")
+    source = FFmpegPCMAudio(executable="ffmpeg.exe", source="msg.mp3")
+    voice.play(source)
+    
+bot.run("ODI0MTk4NDM4ODYxNDA2Mjc4.YFr4nA.D5q9c4gyPXusqt3GqefGDE8z5-w")
+
---
+++ mp3.py
@@ -0,0 +1,3 @@
+from gtts import gTTS
+def dillo_in_napoletano(_str):
+    return gTTS(_str,lang="it")
